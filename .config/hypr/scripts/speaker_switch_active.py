4da019f6b06e420819042da38905a8cbc178de2b
Arnav Pushkarna <kakamishu2012@gmail.com>
2025-04-24 13:20:29 -0700

    bugfixes

--- .config/hypr/scripts/speaker_switch_active.py
+++ .config/hypr/scripts/speaker_switch_active.py
@@ -1,30 +1,17 @@
 import subprocess
 # import time
 
-
 default_sink = subprocess.getoutput(["pactl get-default-sink"])
 sinks_raw = subprocess.getoutput([f"pactl list sinks | grep \"Sink #\""]).split('\n')
-print("pactl list sinks | grep \"{default_sink}\"")
-default_sink_num = int(subprocess.getoutput([f"pactl list short sinks | grep \"{default_sink}\""]).split('\t')[0])
-to_change = -1
-
 
 sinks = []
 for raw_sink_line in sinks_raw:
     sinks.append(int(raw_sink_line.split("#")[1]))
 
-for sink_num in sinks:
-    if sink_num != default_sink_num:
-        # subprocess.run(["pactl", "set-default-sink", str(sink_num)])
-        to_change = sink_num
-
 # time.sleep(2)
 
 current_window_name = subprocess.getoutput(["hyprctl activewindow | grep \"title: \""]).strip().split(": ")[1]
 sink_inputs_info_raw = subprocess.getoutput(["pactl list sink-inputs"]).split('\n')
-
-print(current_window_name)
-
 
 for sink_input_info_raw in sink_inputs_info_raw:
 
@@ -37,7 +24,7 @@ for sink_input_info_raw in sink_inputs_info_raw:
 
     found_sink_input_sink = sink_input_info_raw.find("Sink: ")
     if found_sink_input_sink >= 0:
-        sink_input_sink_num = sink_input_info_raw.split(": ")[1]
+        sink_input_sink_num = int(sink_input_info_raw.split(": ")[1])
         print(f"Found sink: {sink_input_sink_num}")
     
     found_sink_input_name = sink_input_info_raw.find("media.name")
@@ -49,7 +36,20 @@ for sink_input_info_raw in sink_inputs_info_raw:
         if current_window_name in sink_input_name or sink_input_name in current_window_name:
             print(f"Match found {sink_input_name}")
 
+            found_to_change = False
+            to_change = -1
+
+            for sink_num in sinks:
+                print(f"Checking sink {sink_num} with {sink_input_sink_num}: {sink_num != sink_input_sink_num}")
+                if (sink_num != sink_input_sink_num) and (not found_to_change):
+                    # subprocess.run(["pactl", "set-default-sink", str(sink_num)])
+                    to_change = sink_num
+                    print(f"Changing sink from {sink_input_sink_num} to {to_change}")
+                    found_to_change = True
+                    break
+
             if to_change >= 0:
                 subprocess.run(["pactl", "move-sink-input", f"{sink_input_num}", f"{to_change}"])
+                print("pactl", "move-sink-input", f"{sink_input_num}", f"{to_change}")
             
             break
