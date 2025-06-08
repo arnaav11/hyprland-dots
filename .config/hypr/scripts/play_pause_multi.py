fe1194e672a3f3063a88e3c0bdd42aa379be99e8
Arnav Pushkarna <kakamishu2012@gmail.com>
2025-06-08 17:28:47 +0530

    updated handling multiple media players

--- .config/hypr/scripts/play_pause_multi.py
+++ .config/hypr/scripts/play_pause_multi.py
@@ -1,6 +1,7 @@
 import sys, subprocess, time
 
-STATE_ICONS = {'paused': ['media-playback-start', 'Playing'], 'playing': ['media-playback-pause', 'Paused']}
+STATE_DISPLAY = {'paused': ['media-playback-start', 'Playing'], 'playing': ['media-playback-pause', 'Paused']}
+ACTIONS = {'n': 'next', 'next': 'next', 'p': 'previous', 'prev': 'previous', 'previous': 'previous', 'pp': 'play-pause', 'play-pause': 'play-pause'}
 
 args_list = sys.argv[1:]
 players_list = subprocess.getoutput('playerctl -l').split('\n')
@@ -8,16 +9,24 @@ players_states = subprocess.getoutput('playerctl status -a').lower().split('\n')
 
 
 try:
-    if args_list[0] == 'a':
+    selected_action = ACTIONS[args_list[0]]
+    if args_list[1] == 'a':
         args_list = list(range(len(players_list)))
     else:
-        args_list = list(map(lambda x: int(x)-1, args_list))
+        args_list = list(map(lambda x: int(x)-1, args_list[1:]))
 
-    players_states_display = list(map(lambda x: [STATE_ICONS[players_states[x]][0], STATE_ICONS[players_states[x]][1]], range(len(players_list))))    
+    if selected_action == 'next':
+        STATE_DISPLAY['paused'] = ['go-next', 'Going next in']
+        STATE_DISPLAY['playing'] = ['go-next', 'Going next in']
+    elif selected_action == 'previous':
+        STATE_DISPLAY['paused'] = ['go-previous', 'Going previous in']
+        STATE_DISPLAY['playing'] = ['go-previous', 'Going previous in']
+
+    players_states_display = list(map(lambda x: [STATE_DISPLAY[players_states[x]][0], STATE_DISPLAY[players_states[x]][1]], range(len(players_list))))    
 
     for i in args_list:
         if i < len(players_list):
-            subprocess.run(['playerctl', 'play-pause', f'--player={players_list[i]}'])
+            subprocess.run(['playerctl', selected_action, f'--player={players_list[i]}'])
             subprocess.run(['swayosd-client', f'--custom-icon={players_states_display[i][0]}', f'--custom-message={players_states_display[i][1]} {players_list[i].split('.')[0]}'])
             time.sleep(0.5)
 
